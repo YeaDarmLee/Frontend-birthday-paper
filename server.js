dad3d1e2332447da16079fad390b0ae86c6edd2c
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-21 18:42:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 
-const path = require('path');
 const cors = require('cors');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(__dirname, '/dist/index.html');
+});
 
 app.use(cors());
 app.listen(port);
