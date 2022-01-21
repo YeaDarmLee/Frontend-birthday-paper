88f887f44eff97581690fe5d47c10b3b5f39f6fb
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2022-01-21 19:10:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const cors = require('cors');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-const router = require('./src/router/index');
-
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(serveStatic('/write', router));
 
 app.use(cors());
 app.listen(port);
